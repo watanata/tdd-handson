45fa0c7b4eeea4136bbe84745a9dd96433c3fb1d
watanata <watanabetatsuya@lifull.com>
2019-08-20 15:24:27 +0900

    refactor test code

--- fizzbuzz_spec.rb
+++ fizzbuzz_spec.rb
@@ -2,11 +2,13 @@ require_relative './fizzbuzz'
 
 describe FizzBuzz do
   describe '#fizzbuzz' do
-    it 'returns 1 when arg is 1' do
-      expect(subject.fizzbuzz(1)).to eq '1'
-    end
-    it 'returns 7 when arg is 7' do
-      expect(subject.fizzbuzz(7)).to eq '7'
+    context 'when arg is other than multiples of 3 and 5' do
+      it 'returns 1 when arg is 1' do
+        expect(subject.fizzbuzz(1)).to eq '1'
+      end
+      it 'returns 7 when arg is 7' do
+        expect(subject.fizzbuzz(7)).to eq '7'
+      end
     end
   end
 end
